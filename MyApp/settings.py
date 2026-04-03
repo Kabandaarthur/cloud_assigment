29dd9efd00a267d44fe7253788ab7310e29e6727
Kabandaarthur <arthurvardy27@gmail.com>
2026-04-03 07:39:49 +0300

    Allow app to run without .env file in production

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import Config, RepositoryEnv, Csv
+from decouple import config, Csv
 
-# Load environment variables from .env file
-env = Config(RepositoryEnv('.env'))
+# Setup environment variables
+env = config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
